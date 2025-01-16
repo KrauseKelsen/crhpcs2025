5ccf1ba1d9332160aa8035e8097e14ba23135b6c
Christian Asch <casch@cenat.ac.cr>
2025-01-16 09:48:37 -0600

    Reorganized project

--- basics/matmul/SOLUTION/main.cpp
+++ basics/matmul/SOLUTION/main.cpp
@@ -2,38 +2,32 @@
 #include <cstdlib>
 #include <iostream>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   Kokkos::initialize(argc, argv);
   const int width = 2;
   const int height = 2;
-  int *matA = (int*) malloc(sizeof(int) * width * height);
-  int *matB = (int*) malloc(sizeof(int) * width * height);
-  int *matC = (int*) malloc(sizeof(int) * width * height);
+  int *matA = (int *)malloc(sizeof(int) * width * height);
+  int *matB = (int *)malloc(sizeof(int) * width * height);
+  int *matC = (int *)malloc(sizeof(int) * width * height);
 
-  Kokkos::parallel_for(width * height,
-    [=] (int i) {
-      matA[i] = i;
-      matB[i] = 10 - i;
-      matC[i] = 0;
-    });
+  Kokkos::parallel_for(
+      width * height, KOKKOS_LAMBDA(int i) {
+        matA[i] = i;
+        matB[i] = 10 - i;
+        matC[i] = 0;
+      });
 
-  Kokkos::parallel_for(width,
-      [=] (int i)
-      {
-        for(int j = 0; j < height; ++j)
-        {
-          for(int k = 0; k < height; ++k)
-          {
+  Kokkos::parallel_for(
+      width, KOKKOS_LAMBDA(int i) {
+        for (int j = 0; j < height; ++j) {
+          for (int k = 0; k < height; ++k) {
             matC[i * height + j] += matA[i * height + k] * matB[k * height + j];
           }
         }
       });
 
-  for(int i = 0; i < width; ++i)
-  {
-    for(int j = 0; j < height; ++j)
-    {
+  for (int i = 0; i < width; ++i) {
+    for (int j = 0; j < height; ++j) {
       std::cout << matC[i * height + j] << ' ';
     }
     std::cout << '\n';
--- basics/pi_leibniz/SOLUTION/main.cpp
+++ basics/pi_leibniz/SOLUTION/main.cpp
@@ -1,27 +1,23 @@
-#include <Kokkos_Core.hpp>
-#include <cstdlib>
 #include <iostream>
 
-int main(int argc, char **argv)
-{
+#include <Kokkos_Core.hpp>
+
+int main(int argc, char **argv) {
   Kokkos::initialize(argc, argv);
   int total_iterations = -1;
-  if(argc < 2)
-  {
+  if (argc < 2) {
     std::cout << "Usage: " << argv[0] << " <number of iterations>\n";
     std::exit(EXIT_FAILURE);
-  }
-  else
-  {
+  } else {
     total_iterations = std::atoi(argv[1]);
   }
 
   double sum = 0.;
-  Kokkos::parallel_reduce(total_iterations,
-      [=] (const int i, double &partial_sum)
-      {
+  Kokkos::parallel_reduce(
+      total_iterations,
+      KOKKOS_LAMBDA(const int i, double &partial_sum) {
         int sign = i % 2 == 0 ? 1 : -1;
-        partial_sum += sign/(2. * i + 1.);
+        partial_sum += sign / (2. * i + 1.);
       },
       sum);
   std::cout << 4 * sum << '\n';
