9cb529450679463b5c09309ace1cfd74b48a0b4c
Christian Asch <christian.asch4@gmail.com>
2025-01-23 22:53:00 -0600

    Added a heat simulation example

--- basics/1.initialization/main.cpp
+++ basics/1.initialization/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
   std::cout << "******************\n";
   std::cout << "Hello, world!\n";
   for (int i = 0; i < argc; ++i) {
-    std::cout << i << argv[i] << '\n';
+    std::cout << i << ": " << argv[i] << '\n';
   }
   Kokkos::finalize();
 }
