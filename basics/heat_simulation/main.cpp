e1696ebad4fad0d74a45494a5691822a16591f87
Christian Asch <casch@cenat.ac.cr>
2025-01-24 09:28:10 -0600

    Improved simulation example

--- basics/heat_simulation/main.cpp
+++ basics/heat_simulation/main.cpp
@@ -13,7 +13,7 @@
  * @param[in] nx Number of rows in the grid.
  * @param[in] ny Number of columns in the grid.
  */
-void initialize_grid(Kokkos::View<double **> grid, int nx, int ny) {
+void initialize_grid(Kokkos::View<double **> grid, const int nx, const int ny) {
 
   Kokkos::parallel_for(
       "initialize_grid", nx * ny, KOKKOS_LAMBDA(const int idx) {
@@ -34,31 +34,28 @@ void initialize_grid(Kokkos::View<double **> grid, int nx, int ny) {
  * @param[in] nx Number of rows in the grid.
  * @param[in] ny Number of columns in the grid.
  */
-void diffuse_heat(Kokkos::View<double **> current, Kokkos::View<double **> next,
-                  int nx, int ny) {
-  const auto grid_spacing = 0.5;
-  const auto grid_spacing_sq = 1 / (grid_spacing * grid_spacing);
-  const auto delta_t = 0.05;
-  const auto factor = delta_t * grid_spacing_sq;
+void diffuse_heat(Kokkos::View<const double **> current,
+                  Kokkos::View<double **> next, const int nx, const int ny) {
+  constexpr double grid_spacing = 0.5;
+  constexpr double delta_t = 0.05;
+  constexpr double factor = delta_t / (grid_spacing * grid_spacing);
+  const auto grid_policy = Kokkos::MDRangePolicy({1, 1}, {nx - 1, ny - 1});
   Kokkos::parallel_for(
-      "Diffuse Heat", Kokkos::RangePolicy<>(1, nx - 1),
-      KOKKOS_LAMBDA(const int i) {
-        for (int j = 1; j < ny - 1; ++j) {
-          next(i, j) =
-              current(i, j) + factor * (current(i + 1, j) + current(i - 1, j) +
-                                        current(i, j - 1) + current(i, j + 1) -
-                                        4 * current(i, j));
-        }
+      "Diffuse Heat", grid_policy, KOKKOS_LAMBDA(const int i, const int j) {
+        next(i, j) =
+            current(i, j) + factor * (current(i + 1, j) + current(i - 1, j) +
+                                      current(i, j - 1) + current(i, j + 1) -
+                                      4 * current(i, j));
       });
   // Apply boundary conditions: keep the edges at 100 degrees
   Kokkos::parallel_for(
-      "Apply Boundary Conditions", Kokkos::RangePolicy<>(1, nx - 1),
+      "Apply Boundary Conditions", Kokkos::RangePolicy<>(0, nx),
       KOKKOS_LAMBDA(const int i) {
         next(i, 0) = 100.0;
         next(i, ny - 1) = 100.0;
       });
   Kokkos::parallel_for(
-      "Apply Boundary Conditions", Kokkos::RangePolicy<>(1, ny - 1),
+      "Apply Boundary Conditions", Kokkos::RangePolicy<>(0, ny),
       KOKKOS_LAMBDA(const int j) {
         next(0, j) = 100.0;
         next(nx - 1, j) = 100.0;
@@ -79,8 +76,8 @@ void diffuse_heat(Kokkos::View<double **> current, Kokkos::View<double **> next,
  * @param ny Number of grid points in y-direction.
  * @param step Current time step number.
  */
-void output_heat_distribution(const Kokkos::View<double **> current, int nx,
-                              int ny, int step) {
+void output_heat_distribution(const Kokkos::View<const double **> current,
+                              const int nx, const int ny, const int step) {
   std::ofstream file;
   std::string filename = "heat_" + std::to_string(step) + ".txt";
   auto host_current =
@@ -107,8 +104,8 @@ void output_heat_distribution(const Kokkos::View<double **> current, int nx,
  * @param ny Number of grid points in y-direction.
  * @param step Current time step
  */
-void output_vtk(const Kokkos::View<double **> current, int nx, int ny,
-                int step) {
+void output_vtk(Kokkos::View<const double **> current, const int nx,
+                const int ny, const int step) {
   std::ofstream file;
   std::string filename = "heat_" + std::to_string(step) + ".vtk";
   auto host_current =
@@ -146,13 +143,16 @@ int main(int argc, char **argv) {
   int nx = 100;    // Number of grid points in x direction
   int ny = 100;    // Number of grid points in y direction
   int steps = 500; // Total number of time steps
-  if (argc < 4) {
-    std::cerr << "Usage: " << argv[0] << " <nx> <ny> <steps>\n";
+  int output_freq = 1000;
+  if (argc < 5) {
+    std::cerr << "Usage: " << argv[0]
+              << " <nx> <ny> <steps> <output frequency>\n";
     exit(-1);
   } else {
     nx = atoi(argv[1]);
     ny = atoi(argv[2]);
     steps = atoi(argv[3]);
+    output_freq = atoi(argv[4]);
   }
   {
     Kokkos::View<double **> heat_map("heat_map", nx, ny);
@@ -163,15 +163,30 @@ int main(int argc, char **argv) {
 
     std::cout << "Starting simulation...\n";
     // Perform the heat simulation for the specified number of steps
+    auto timer = Kokkos::Timer();
+    double total_step_time = 0.;
+    double total_io_time = 0.;
     for (int step = 0; step < steps; ++step) {
+      timer.reset();
       diffuse_heat(heat_map, new_heat_map, nx, ny);
+      total_step_time += timer.seconds();
       // Swap the views to use the updated heat distribution in the next
       // iteration
       Kokkos::kokkos_swap(heat_map, new_heat_map);
-      output_vtk(heat_map, nx, ny, step);
+      if (step % output_freq == 0) {
+        std::cout << "Writing step " << step << '\n';
+        timer.reset();
+        output_vtk(heat_map, nx, ny, step);
+        total_io_time += timer.seconds();
+      }
     }
-    std::cout << "Simulation complete, outputting results...\n";
-    // Output the final heat distribution
+    auto average_step_time = total_step_time / steps;
+    auto average_io_time = total_io_time / steps;
+    std::cout << "Simulation complete...\n";
+    std::cout << "Total step time: " << total_step_time << '\n';
+    std::cout << "Average step time: " << average_step_time << '\n';
+    std::cout << "Total I/O time: " << total_io_time << '\n';
+    std::cout << "Average I/O time: " << average_io_time << '\n';
   }
   Kokkos::finalize();
   return 0;
