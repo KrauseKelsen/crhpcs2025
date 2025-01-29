22704306fd8d928b0065aaef45dec8c7c4f614ec
Christian Asch <casch@cenat.ac.cr>
2025-01-29 16:56:21 -0600

    Fixed solution for findpi.cpp

--- distributed_memory/A_solutions/findpi.cpp
+++ distributed_memory/A_solutions/findpi.cpp
@@ -1,5 +1,6 @@
 #include "mpi.h"
 #include <math.h>
+#include <cstdlib>
 
 #define PI25DT 3.141592653589793238462643
 
@@ -12,18 +13,18 @@ int main(int argc, char* argv[])
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-  if (rank == 0) {
-    printf("Enter the number of intervals: (0 is invalid) ");
-    scanf("%d",&n);
-  }
+  n = atoi(argv[1]);
+
 
   // we need to have the number of intervals in all our ranks
   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   h   = 1.0 / (double) n; // width of subdivision
   sum = 0.0;              // initialize sum to zero
+  double start = (n/size) * rank;
+  double stop = (n/size) * (rank + 1);
   
-  for (i = rank + 1; i <= n; i += size) {
+  for (i = start; i <= stop; i += 1) {
     x = h * ((double)i - 0.5);  // midpoint calculation
     sum += 4.0 / (1.0 + x*x);
   }
